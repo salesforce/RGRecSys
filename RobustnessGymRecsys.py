58a8eb5bcc135b5ab2896ad74225ae13c8c61c5f
Shelby Heinecke <shelby.heinecke@salesforce.com>
2022-01-11 13:16:45 -0800

    add code of conduct, license, security; add license to all pages

--- RobustnessGymRecsys.py
+++ RobustnessGymRecsys.py
@@ -1,3 +1,11 @@
+"""
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+"""
 from utils.GlobalVars import *
 from recbole.config import Config, EvalSetting
 from recbole.sampler import Sampler, RepeatableSampler, KGSampler
